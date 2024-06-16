c86d3bfaab32a02cd25c1f727c2ee2940b796bba
Nishchey khajuria <nishcheycapture2014@gmail.com>
2024-06-17 02:01:29 +0530

    done some work of steam

---
+++ providers/StreamClientProvider.tsx
@@ -0,0 +1,29 @@
+import { StreamVideoClient, StreamVideo } from "@stream-io/video-react-sdk";
+import { ReactNode, useEffect, useState } from "react";
+import { useUser } from "@clerk/nextjs";
+
+import { tokenProvider } from '@/actions/stream.actions';
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+
+const StreamVideoProvider = ({ childern }: { childern: ReactNode }) => {
+  const [videoClient, setVideoClient] = useState<StreamVideoClient>();
+  const { user, isLoaded } = useUser();
+
+  useEffect(() => {
+    if (!isLoaded || !user) return;
+    if (!apiKey) throw new Error("Stream API key missing");
+
+    const client = new StreamVideoClient({
+      apiKey:api,
+      user: {
+        id: user?.id,
+        name: user?.username || user?.id,
+        image: user?.imageUrl,
+      },
+      tokenProvider,
+    });
+  }, [user, isLoaded]);
+
+  return <StreamVideo client={videoClient}></StreamVideo>;
+};
+export default StreamVideoProvider;
